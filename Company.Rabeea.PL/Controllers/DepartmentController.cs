82ab2aafc0b96c526c5ebe27a00e2c97ce487443
ARabee3 <arabeea7104@gmail.com>
2025-03-14 16:37:09 +0200

    Deapartment Controller Created

---
+++ Company.Rabeea.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using Company.Rabeea.BLL.Interfaces;
+using Company.Rabeea.BLL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Rabeea.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // Ask CLR To Create Object of DepartmentRepository
+        private readonly IDepartmentRepository _departmentRepository;
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+        public IActionResult Index()
+        { 
+            var departments = _departmentRepository.GetAll();
+            return View(departments);
+        }
+    }
+}
