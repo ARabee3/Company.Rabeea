8ef16274fc8f20ed2cf30125915f3d8ddbf5ec0b
ARabee3 <arabeea7104@gmail.com>
2025-03-16 02:47:29 +0200

    Create Create (Action View)

---
+++ Company.Rabeea.PL/Dto/CreateDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Company.Rabeea.PL.Dto;
+
+public class CreateDepartmentDto
+{
+    [Required(ErrorMessage = "Code is Required!")]
+    public string Code { get; set; }
+    [Required(ErrorMessage = "Name is Required!")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "CreateAt is Required!")]
+    public DateTime CreateAt { get; set; }
+}
