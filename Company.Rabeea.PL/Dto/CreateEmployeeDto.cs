4cc0b0c309c7c0360457c97cf8c526d0eb6e1d52
ARabee3 <arabeea7104@gmail.com>
2025-03-20 12:48:34 +0200

    PL For Employee

---
+++ Company.Rabeea.PL/Dto/CreateEmployeeDto.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace Company.Rabeea.PL.Dto;
+
+public class CreateEmployeeDto
+{
+    [Required(ErrorMessage = "Name is Required!")]
+    public string Name { get; set; }
+
+
+    [Range(22,60,ErrorMessage ="Age Must Be Between 22 & 60")]
+    public int? Age { get; set; }
+
+
+    [Required(ErrorMessage = "Email is Required!")]
+    [DataType(DataType.EmailAddress , ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; }
+
+
+    [Required(ErrorMessage = "Address is Required!")]
+    [RegularExpression(@"[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$",
+        ErrorMessage ="Address Format is '123-Street-city-country'")]
+    public string Address { get; set; }
+
+
+    [Phone]
+    public string Phone { get; set; }
+    
+
+    [Required(ErrorMessage = "Salary is Required!")]
+    [DataType(DataType.Currency)]
+    public decimal Salary { get; set; }
+
+    public bool IsActive { get; set; }
+
+    [Required(ErrorMessage = "HiringDate is Required!")]
+    [DisplayName("Hiring Date")]
+    public DateTime HiringDate { get; set; }
+    [Required(ErrorMessage = "CreateAt is Required!")]
+    [DisplayName("Date Of Creation")]
+
+    public DateTime CreateAt { get; set; }
+}
